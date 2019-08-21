0baba0a2cb6b6d792f61c9662504371839c07353
pongpol-j <53513779+pongpol-j@users.noreply.github.com>
2019-08-21 14:40:35 +0700

    Create JSX2.js

---
+++ JSX2.js
@@ -0,0 +1,167 @@
+import * as React from 'react';
+import { Text, View, StyleSheet,ScrollView,TouchableOpacity,TextInput,Alert } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+let names=[];
+let math = [];
+let phy = [];
+let com = [];
+
+export default class App extends React.Component {
+
+  onNameChange(index,text){
+    names[index]=text;
+  }
+  onMathChange(index,text){
+    math[index]=text;
+  }
+  onPhyChange(index,text){
+    phy[index]=text;
+  }
+  onComChange(index,text){
+    com[index]=text;
+  }
+
+  onCalculate(){
+    let i;
+    let sum=0;
+    let index=0;
+    let max = 0
+    for(i=0; i<3 ; i++)
+    {
+      sum += math[i] + phy[i] + com[i];
+      if(sum > max)
+      {
+        index=i;
+        max=sum;
+      }
+      sum=0;
+    }
+    alert(names[index]+max)
+  }
+
+  render() {
+    return (
+      <LinearGradient
+       colors={['#7F7FD5', '#86A8E7', '#91EAE4']}
+       style={{flex: 1}}>
+        <ScrollView>
+          <View style={{flex: 1}}>
+
+              <Text style={styles.txtShow}> Student1 </Text>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Name"
+                onChangeText={(text) => {this.onNameChange(0,text)}}/>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Mathematics"
+                onChangeText={(text) => {this.onMathChange(0,Number(text))}}/>
+
+              <TextInput
+                style={styles.inputText}
+                placeholder="Physic"
+                onChangeText={(text) => {this.onPhyChange(0,Number(text))}}/>
+
+              <TextInput
+                style={styles.inputText}
+                placeholder="Computer"
+                onChangeText={(text) => {this.onComChange(0,Number(text))}}/>
+
+          </View>
+          
+          <View style={{flex: 1}}>
+
+              <Text style={styles.txtShow}> Student2 </Text>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Name"
+                onChangeText={(text) => {this.onNameChange(1,text)}}/>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Mathematics"
+                onChangeText={(text) => {this.onMathChange(1,Number(text))}}/>
+
+              <TextInput
+                style={styles.inputText}
+                placeholder="Physic"
+                onChangeText={(text) => {this.onPhyChange(1,Number(text))}}/>
+
+              <TextInput
+                style={styles.inputText}
+                placeholder="Computer"
+                onChangeText={(text) => {this.onComChange(1,Number(text))}}/>
+
+          </View>
+          
+          <View style={{flex: 1}}>
+
+              <Text style={styles.txtShow}> Student3 </Text>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Name"
+                onChangeText={(text) => {this.onNameChange(2,text)}}/>
+              <TextInput
+                style={styles.inputText}
+                placeholder="Mathematics"
+                onChangeText={(text) => {this.onMathChange(2,Number(text))}}/>
+
+              <TextInput
+                style={styles.inputText}
+                placeholder="Physic"
+                onChangeText={(text) => {this.onPhyChange(2,Number(text))}}/>
+
+              <TextInput
+                style={styles.inputText}
+                placeholder="Computer"
+                onChangeText={(text) => {this.onComChange(2,Number(text))}}/>
+      
+              <TouchableOpacity
+                style={styles.touchableUser}
+                onPress={() => this.onCalculate()}>
+                <Text style={styles.txtButton}>Calculate</Text>
+              </TouchableOpacity>
+
+          </View>
+        </ScrollView>
+      </LinearGradient>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  txtShow:{
+     margin:5,
+     fontSize : 20,
+     color: 'white'
+  },
+  txtButton:  {
+    fontSize:20, 
+    color:'#ffffff',    
+    textAlign:'center'
+  },
+  inputText: {
+      alignItems: 'center',
+      height:50,
+      backgroundColor: 'transparent',
+      padding: 10,
+      margin:5,
+      marginLeft:10,
+      marginEnd:10,
+      borderRadius: 50,
+      borderColor:'white',
+      borderWidth : 1,
+      fontSize:20,
+  },
+  touchableUser:{
+    alignItems: 'center',
+    padding:10,
+    borderRadius: 50,
+    borderColor:'white',
+    borderWidth : 1,
+    margin:5,
+    marginLeft:10,
+    marginEnd:10,
+    marginTop:25,
+  }
+});
